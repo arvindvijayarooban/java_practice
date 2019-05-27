06b279db28ec55529e87719fc87a7f9ed5700c5a
Varsha Tomy <varsha.tomy@baesystems.com>
2019-05-27 22:09:14 +0800

    Committing ExampleThree

---
+++ src/ExampleThree.java
@@ -0,0 +1,12 @@
+public class ExampleThree {
+
+
+   public void testExample3() {
+
+       int a[][] = new int[3][];
+       a[1] = new int[]{1,2,3};
+       a[2] = new int [] {4,5};
+       System.out.println(a[1][1]);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,38 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int x =10;
-        int y = 2;
+//        int x =10;
+//        int y = 2;
+//
+//        try{
+//            for(int z =2 ; z >=0; z--){
+//                int ans = x/z;
+//                System.out.println(ans + " ");
+//
+//            }
+//
+//        }catch( Exception e1){
+//            System.out.println(e1);
+//    }
+//        ExampleTwo e2 = new ExampleTwo();
+//        e2.testAppend();
+//
+//        ExampleThree e3 = new ExampleThree();
+//        e3.testExample3();
+//
+//        int []a = {1,2,3,4};
+//        System.out.println(a instanceof Object);
+//
+//        int x =10;
+//        int y = new Main().change(x);
+//        System.out.print(x+y);
+//
+//    }
+//
+//    int change(int x){
+//
+//        x =12;
+//        return x;
+//    }
 
-        try{
-            for(int z =2 ; z >=0; z--){
-                int ans = x/z;
-                System.out.println(ans + " ");
-
-            }
-
-        }catch( Exception e1){
-            System.out.println(e1);
-
-        }
-
-        ExampleTwo e2 = new ExampleTwo();
-        e2.testAppend();
-    }
 }
