e1a643fd366a571915028fb59203f30190530ceb
Varsha Tomy <varsha.tomy@baesystems.com>
2019-05-27 21:25:41 +0800

    Committing ExampleTwo

---
+++ src/ExampleTwo.java
@@ -0,0 +1,14 @@
+public class ExampleTwo {
+
+
+void testAppend () {
+    StringBuilder s1 = new StringBuilder("Java");
+    String s2 = "Love";
+    s1.append(s2);
+    s1.substring(4);
+    int foundAt = s1.indexOf(s2);
+    System.out.println(foundAt);
+    System.out.println(s1.substring(4));
+}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int x =10;
+        int y = 2;
+
+        try{
+            for(int z =2 ; z >=0; z--){
+                int ans = x/z;
+                System.out.println(ans + " ");
+
+            }
+
+        }catch( Exception e1){
+            System.out.println(e1);
+
+        }
+
+        ExampleTwo e2 = new ExampleTwo();
+        e2.testAppend();
     }
 }
